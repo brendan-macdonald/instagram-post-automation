470e53bc5587627f5975b8b670db038785772036
brendan-macdonald <macdonald.brendan@outlook.com>
2025-05-13 20:38:09 -0500

    intial project setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+//  main entry point for the application
+// sets up the Express server and connects to the database
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// basic route to check if the server is running
+app.get("/", (req, res) => {
+  res.send("Tiiktok to Instagram Automation is running");
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
