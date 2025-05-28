4de00e5c3365c8536bd4d6525e284ce54883b546
brendan-macdonald <macdonald.brendan@outlook.com>
2025-05-28 17:52:49 -0500

    Test and log JSON results from tikwm api

--- utils/downloadTikTok.js
+++ utils/downloadTikTok.js
@@ -1,22 +1,22 @@
-const puppeteer = require("puppeteer");
-// const fs = require('fs');
-// const https = require('https');
-// const path = require('path');
+const axios = require("axios");
 
-// Step 1: Open snapTik.app and print the page title
-async function downloadTikTokVideo(url, outputPath) {
-  const browser = await puppeteer.launch({ headless: "false" });
-  const page = await browser.newPage();
-
-  console.log("Navigating to snaptik.app...");
-  await page.goto("https://snaptik.app", {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  });
-
-  const title = await page.title();
-  console.log("Page title:", title);
-  await browser.close();
+async function downloadTikTokVideo(videoUrl) {
+  try {
+    // Make a GET request to the TikTok video URL
+    const response = await axios.get("https://tikwm.com/api/", {
+      params: {
+        url: videoUrl,
+        no_watermark: true, // Set to true to download without watermark
+      },
+    });
+    console.log("Response:", response.data);
+    // Check if the response contains the video URL
+    console.dir(response.data, { depth: null });
+  } catch (error) {
+    console.error("Error downloading TikTok video:", error.message);
+  }
 }
 
-module.exports = { downloadTikTokVideo };
+module.exports = {
+  downloadTikTokVideo,
+};
