9d3ec774e4c816bc5e5f6b2f32dc3a95dcce29e4
brendan-macdonald <macdonald.brendan@outlook.com>
2025-05-28 17:52:49 -0500

    Test and log JSON results from tikwm api

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { downloadTikTokVideo } = require("./utils/downloadTikTok");
 
-(async () => {
-  const videoUrl = "https://www.tiktok.com/@example/video/1234567890"; // Replace with your TikTok video URL
-  const outputPath = "downloads/test.mp4"; // Path where you want to save the video
-  await downloadTikTokVideo(videoUrl, outputPath);
-  console.log("Video downloaded successfully!");
-})();
+const testUrl = "https://www.tiktok.com/@mrbeast/video/7505849400019717406";
+
+downloadTikTokVideo(testUrl);
