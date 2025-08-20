cc4c138bf46ef553a1581f3bb9ffaf0fb4ea7aec
brendan-macdonald <macdonald.brendan@outlook.com>
2025-08-20 12:20:52 -0500

    twitter implemenation start including db changes

--- utils/getNextUnprocessed.js
+++ utils/getNextUnprocessed.js
@@ -1,16 +1,13 @@
+// utils/getNextUnprocessed.js
 const sqlite3 = require("sqlite3").verbose();
 
-// This function retrieves the next unprocessed TikTok video from the database
-// that has not been downloaded or posted yet.
-function getNextUnprocessedTikTok(dbPath, callback) {
+function getNextUnprocessed(dbPath, callback) {
   const db = new sqlite3.Database(dbPath);
   db.get(
-    `
-    SELECT * FROM tiktoks
-    WHERE posted = 0
-    ORDER BY downloaded DESC, created_at ASC
-    LIMIT 1
-    `,
+    `SELECT * FROM media_queue
+     WHERE posted = 0
+     ORDER BY downloaded DESC, created_at ASC
+     LIMIT 1`,
     (err, row) => {
       db.close();
       callback(err, row);
@@ -18,4 +15,4 @@ function getNextUnprocessedTikTok(dbPath, callback) {
   );
 }
 
-module.exports = { getNextUnprocessedTikTok };
+module.exports = { getNextUnprocessed };
